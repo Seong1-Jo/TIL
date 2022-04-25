8e17efb45dcff5374b7b50c1cf95995cdd46d81d
Seong1-Jo <seong12yada@gmail.com>
2022-04-25 22:39:24 +0900

    fix: Add Semicolon forEach.js
    LastPart Add Semicolon

--- JavaScript/220425-JS-Method-forEach.js
+++ JavaScript/220425-JS-Method-forEach.js
@@ -4,4 +4,4 @@ const boxEls = document.querySelectorAll('.box');//HTML tags
 boxEls.forEach(function (boxEl, index) { 
   boxEl.classList.add(`order-${index + 1}`); // Add ClassName 
   console.log(boxEl, index);
-})
+});
